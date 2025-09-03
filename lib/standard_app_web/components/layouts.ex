383e461cce09fca21389c0d52a656d722031b4be
Varun Shivakumar <fortfinchrpg@gmail.com>
2025-09-03 15:46:59 +1000

    refactored app layouts into shared component

--- lib/standard_app_web/components/layouts.ex
+++ lib/standard_app_web/components/layouts.ex
@@ -35,44 +35,30 @@ defmodule StandardAppWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
-        </a>
-      </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
-          <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </header>
-
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
-        {render_slot(@inner_block)}
-      </div>
-    </main>
-
-    <.flash_group flash={@flash} />
+    <.app_layout flash={@flash}>
+      {render_slot(@inner_block)}
+    </.app_layout>
     """
   end
 
   def app_static(assigns) do
+    ~H"""
+    <.app_layout flash={@flash}>
+      {@inner_content}
+    </.app_layout>
+    """
+  end
+
+  @doc """
+  Shared application layout component used by both app and app_static layouts.
+
+  This component provides the common header, navigation, and main content area
+  that both LiveView and controller layouts share.
+  """
+  attr :flash, :map, required: true, doc: "the map of flash messages"
+  slot :inner_block, required: true, doc: "the content to render in the main area"
+
+  def app_layout(assigns) do
     ~H"""
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
@@ -103,7 +89,7 @@ defmodule StandardAppWeb.Layouts do
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl space-y-4">
-        {@inner_content}
+        {render_slot(@inner_block)}
       </div>
     </main>
 
